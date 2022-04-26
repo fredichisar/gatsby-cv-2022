a2f335bd69fa976cdc0738358194e960d9a96828
Fred <frederic.ichisar@ledger.fr>
2022-04-26 15:16:37 +0200

    Add sourcesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
